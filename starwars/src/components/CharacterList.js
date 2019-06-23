f15421f7b8c174cc1bab3ff06605c45f602c2cd9
Kevin-S <kevsilvo@gmail.com>
2019-06-23 19:48:29 -0400

    eliminated-keys warning

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -2,7 +2,7 @@
  import Character from './Character';
 
  const CharacterList = props => {
-    return <div>{props.characterProps.map(c => <Character name={c} key={c.id} />)}</div>;
+    return <div>{props.characterProps.map(c => <Character name={c} key={c.created} />)}</div>;
   };
   
 
