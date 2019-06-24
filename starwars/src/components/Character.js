933c10ae4b01c05924414c2dabc51ae9ce00c790
Kevin-S <kevsilvo@gmail.com>
2019-06-23 20:13:33 -0400

    creating-films

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,16 +1,25 @@
 import React from "react";
+import Character_Films from './CharacterFilms';
 import './StarWars.css';
 
-const Character = props => {
-    console.log("From character", props);
-  return (
-    <div className="character">
-       <h1>Name: {props.name.name}</h1> 
-       <p>Gender: {props.name.gender}</p>
-       <p>birth_year: {props.name.birth_year}</p>
-       <p>height: {props.name.height} |  mass:  {props.name.mass}</p>
-    </div>
-  );
+class Character extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+ 
+render(){
+    return (
+        <div className="character">
+            <h1>Name: {props.name.name}</h1> 
+            <p>Gender: {props.name.gender}</p>
+            <p>birth_year: {props.name.birth_year}</p>
+            <p>height: {props.name.height} |  mass:  {props.name.mass}</p>
+            <Character_Films filmList={props.name.films} />
+        </div>
+    );
 };
+}
+
 
 export default Character;
---
+++ starwars/src/components/CharacterFilms.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CharacterFilms = props =>{
+
+    return(
+
+
+    );
+};
+
+
+
+export default CharacterFilms;
